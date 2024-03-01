3160c32735f7dfa66de2498e0d33b36dd54fb2d1
Your Name <you@example.com>
2024-02-29 21:28:40 -0500

    Tarjetas contacto

--- routes/index.js
+++ routes/index.js
@@ -10,12 +10,19 @@ router.get('/', async (request, response) => {
         response.render('main', { adminData: adminData, errors: errors }); 
     } catch (error) {
         console.error(error);
-        response.render('main', { layout: 'index', errors: ["Error al cargar los datos, recargue la página."] });
+        response.render('main', { errors: ["Error al cargar los datos, recargue la página."] });
     }
 });
 
 router.get('/contacto', async (request, response) => {
-    response.render('contacto', { layout: 'index' });
+    try {
+        const adminResponse = await axios.get('https://4dbvkk2b12.execute-api.us-east-1.amazonaws.com/dev/tasafacil/listar_parametros');
+        const adminData = adminResponse.data;
+        response.render('contacto', { adminData: adminData });
+    } catch (error) {
+        console.error(error);
+        response.render('contacto', { errors: ["Error al cargar los datos, recargue la página."] });
+    }
 });
 
 module.exports = router;
